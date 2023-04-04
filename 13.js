a9c252bc23b616ccc380471f342ebc547186f519
desaimayur125 <112793132+desaimayur125@users.noreply.github.com>
2023-04-04 17:23:56 +0530

    Delete 13.js

--- 13.js
+++
@@ -1,6 +0,0 @@
-function addCommasToNumber(inputNumber) {
-    return inputNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-  const inputNumber = 1023165454197;
-const outputString = addCommasToNumber(inputNumber);
-console.log(outputString);

8e46848945df757a547a715cb3a4a4522252fb25
Mayur Desai <112793132+desaimayur125@users.noreply.github.com>
2024-12-29 17:55:29 +0530

    Delete JS-Exercise directory

--- JS-Exercise/JS-Exercise-2/7.js
+++
@@ -1,9 +0,0 @@
-//7
-function getMonthNameFromDate(dateString) {
-    const dateParts = dateString.split(/[-/]/);
-    const monthIndex = parseInt(dateParts[1], 10) -1 ;
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    return monthNames[monthIndex];
-  }
-  
-  console.log(getMonthNameFromDate('01-07-2020'));
--- JS-Exercise/JS-Exercise-3/Task-1/3.js
+++
@@ -1,7 +0,0 @@
-
-        let str = '<p><strong><em>Javascript Exercises</em></strong></p>';
-       function removetags(str){
-        return str.replace(/<.*?>/g,'');
-       }
-       console.log(removetags(str))
- 
